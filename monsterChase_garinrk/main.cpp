9323e52a0900bbe1eb368342649c60c3238f3063
garinrk <garinrk@gmail.com>
2016-09-01 23:24:36 -0600

    quit flag implemented

--- monsterChase_garinrk/main.cpp
+++ monsterChase_garinrk/main.cpp
@@ -2,24 +2,51 @@
 #include <string>
 using namespace std;
 
-bool askingForInfo = true;
+//forward declarations
+bool CheckForNameValidity(string input);
+bool CheckForNumberValidity(string input);
+void ExitGame();
 
+bool askingForPlayerInfo = true;
+bool askingForNumberOfMonsters = true;
+bool inMainGameplayLoop = true;
+bool quitGameFlag = false;
 string GetConsoleInput();
 
 string userInput;
 bool validCheck;
-bool CheckForValidity(string input);
 int maxUserInputLength = 255;
-
+string welcomeMessage1 = "=======================================";
+string welcomeMessage2 = "==============MONSTER CHASE============";
+string welcomeMessage3 = "================Q to quit=============";
 
 int main() {
 
+	cout << welcomeMessage1 << endl << welcomeMessage2 << endl << welcomeMessage3 << endl;
 
-	while (askingForInfo){ 
-		std::cout << "Enter stuff: ";
+	while (askingForPlayerInfo){ 
+		cout << "Please enter your name (Letters only): ";
 		userInput = GetConsoleInput();
-		validCheck = CheckForValidity(userInput);
-		cout << "Input: " << userInput << " Valid: " << validCheck << endl;
+		validCheck = CheckForNameValidity(userInput);
+		if (quitGameFlag)
+			return 0;
+		if (validCheck == 1) 
+			askingForPlayerInfo = false;
+	}
+
+	while (askingForNumberOfMonsters) {
+		cout << "How many monsters should we start with?: ";
+		userInput = GetConsoleInput();
+		if (quitGameFlag)
+			return 0;
+		validCheck = CheckForNumberValidity(userInput);
+
+		if (validCheck == 1)
+			askingForNumberOfMonsters = false;
+	}
+
+	while (inMainGameplayLoop) {
+
 	}
 
 	return 0;
@@ -33,10 +60,13 @@ string GetConsoleInput()
 
 	getline(cin, r);
 
+	if (r[0] == 'Q' || 'q')
+		quitGameFlag = true;
+
 	return r;
 }
 
-bool CheckForValidity(string input) {
+bool CheckForNameValidity(string input) {
 
 
 	if (static_cast<int>(input.size()) > maxUserInputLength) {
@@ -52,4 +82,27 @@ bool CheckForValidity(string input) {
 	}
 
 	return true;
+}
+
+
+bool CheckForNumberValidity(string input) {
+
+	if (static_cast<int>(input.size()) > maxUserInputLength) {
+		return false;
+
+		//check for digits
+		for (unsigned int i = 0; i < input.length(); i++) {
+			char c = input[i];
+
+			if (!isdigit(c))
+				return false;
+		}
+
+		return true;
+	}
+}
+
+void ExitGame() {
+	cout << "Goodbye!" << endl;
+	
 }
