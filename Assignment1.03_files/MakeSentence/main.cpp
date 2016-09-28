ae9f677e39a523c116a9988c2d4a340108737636
garinrk <garinrk@gmail.com>
2016-09-28 10:21:22 -0600

    Counting the amount of characters in a string :

--- Assignment1.03_files/MakeSentence/main.cpp
+++ Assignment1.03_files/MakeSentence/main.cpp
@@ -1,8 +1,49 @@
 #include <stdio.h>
 #include <conio.h>
+#include <stdlib.h>
+
+char * test(const char ** input);
 
 void main(int i_argc, char ** i_argl) {
 	printf("Hello world!");
+	const char * strings[] = {
+		"This",
+		"is",
+		"a",
+		"test",
+		NULL
+	};
+	char * result = test(strings);
+
+	printf("The Sentence is: %s", result);
+
+	free(result);
 
 	_getch();
+}
+
+char * test(const char ** input) {
+
+	char * r;
+	
+	char currentChar = 0;
+	int currentWord = 0;
+
+	int numberOfChars = 0;
+
+	while (input[currentWord] != NULL) {
+		while (input[currentWord][currentChar] != '\0') {
+			numberOfChars++;
+			currentChar++;
+
+		}
+		currentWord++;
+		currentChar = 0;
+
+	}
+
+	_getch();
+
+	return r;
+
 }
