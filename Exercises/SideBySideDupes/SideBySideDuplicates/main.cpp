29ed7e8d92295e2c36c7484c369c4e1865c29f63
garinrk <garinrk@gmail.com>
2016-11-28 22:38:17 -0700

    Created exercise project for 1.11

---
+++ Exercises/SideBySideDupes/SideBySideDuplicates/main.cpp
@@ -0,0 +1,17 @@
+#include <conio.h>  
+#include <ctype.h>  
+#include <stdio.h>
+
+
+//Remove side-by-side duplicate characters in a string:
+//("AAA BBB" becomes "A B") in place.
+
+int main(int i_argc, char ** i_argl) {
+
+	printf("Hello World\n");
+
+	_getch();
+
+
+	return 0;
+}
