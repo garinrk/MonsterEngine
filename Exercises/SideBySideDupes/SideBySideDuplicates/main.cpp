420f56c80e3d50e8c37037ac8719c3bc6301e290
garinrk <garinrk@gmail.com>
2016-11-29 17:53:05 -0700

    more string swtiching

--- Exercises/SideBySideDupes/SideBySideDuplicates/main.cpp
+++ Exercises/SideBySideDupes/SideBySideDuplicates/main.cpp
@@ -1,17 +1,47 @@
 #include <conio.h>  
 #include <ctype.h>  
+#include <iostream>
 #include <stdio.h>
+#include <string>
 
 
+std::string ReverseString(std::string i_phrase);
 //Remove side-by-side duplicate characters in a string:
 //("AAA BBB" becomes "A B") in place.
 
 int main(int i_argc, char ** i_argl) {
 
-	printf("Hello World\n");
+	std::string input = "Richards";
 
-	_getch();
+	std::string r = ReverseString(input);
 
+	std::cout << "Result: " << r << std::endl;
 
 	return 0;
+}
+
+
+std::string ReverseString(std::string o_phrase) {
+
+	std::cout << "Reversing: " << o_phrase << std::endl;
+	char temp;
+	size_t front = 0;
+	size_t back = o_phrase.length() - 1;
+
+	while(true){
+		temp = o_phrase[front];
+
+		o_phrase[front] = o_phrase[back];
+
+		o_phrase[back] = temp;
+
+		front++;
+		back--;
+
+		if (front == back)
+			break;
+	}
+
+	return o_phrase;
+	
 }
