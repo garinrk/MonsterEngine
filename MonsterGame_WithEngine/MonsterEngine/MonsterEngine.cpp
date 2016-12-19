35af106260052b0765f99ffc4ad6eee214405add
garinrk <garinrk@gmail.com>
2016-12-18 19:38:40 -0700

    Alignment support within new operators, also needed to comment them out as they are defined in the readme.txt at: "MonsterGame_WithEngine/MonsterEngine/ReadMe.txt"

--- MonsterGame_WithEngine/MonsterEngine/MonsterEngine.cpp
+++ MonsterGame_WithEngine/MonsterEngine/MonsterEngine.cpp
@@ -10,60 +10,70 @@ int MMEngine::RandomTrueOrFalse()
 	srand((unsigned int)time(NULL));
 	return rand() % 2;
 }
-
-//regular new using our allocator
-void * operator new(size_t n)
-{
-
-	return MemoryManager::GetInstance()->Malloc(n);
-}
-
-//support for debug logs
-void * operator new(size_t n, const char * msg) {
-	DEBUGLOG(msg);
-
-	return MemoryManager::GetInstance()->Malloc(n);
-
-}
-
-//delete using our allocator
-void operator delete(void * p)
-{
-
-	bool result = MemoryManager::GetInstance()->Free(p);
-	assert(result);
-}
-
-//delete using debug log, will never get called. All funnels down to regular 
-//delete functions
-void operator delete(void * p, const char * msg)
-{
-	DEBUGLOG(msg);
-	bool result = MemoryManager::GetInstance()->Free(p);
-	assert(result);
-}
-
-//regular new[] using our allocator
-void * operator new[](size_t n)
-{
-
-	return MemoryManager::GetInstance()->Malloc(n);
-
-	return nullptr;
-}
-
-//delete[] using our allocator
-void operator delete[](void * p)
-{
-	bool result = MemoryManager::GetInstance()->Free(p);
-	assert(result);
-}
-
-//delete using debug log, won't get called.
-void operator delete[](void * p, const char * msg) {
-	DEBUGLOG(msg);
-	bool result = MemoryManager::GetInstance()->Free(p);
-	assert(result);
-	
-}
+//
+////regular new using our allocator
+//void * operator new(size_t n)
+//{
+//
+//	return MemoryManager::GetInstance()->Malloc(n);
+//}
+//
+////support for debug logs
+//void * operator new(size_t n, const char * msg) {
+//	DEBUGLOG(msg);
+//
+//	return MemoryManager::GetInstance()->Malloc(n);
+//
+//}
+//
+//void * operator new(size_t n, const uint8_t alignment)
+//{
+//	return MemoryManager::GetInstance()->Malloc(n, alignment);
+//}
+//
+////delete using our allocator
+//void operator delete(void * p)
+//{
+//
+//	bool result = MemoryManager::GetInstance()->Free(p);
+//	assert(result);
+//}
+//
+////delete using debug log, will never get called. All funnels down to regular 
+////delete functions
+//void operator delete(void * p, const char * msg)
+//{
+//	DEBUGLOG(msg);
+//	bool result = MemoryManager::GetInstance()->Free(p);
+//	assert(result);
+//}
+//
+////regular new[] using our allocator
+//void * operator new[](size_t n)
+//{
+//
+//	return MemoryManager::GetInstance()->Malloc(n);
+//
+//	return nullptr;
+//}
+//
+//void * operator new[](size_t n, const uint8_t alignment)
+//{
+//	return MemoryManager::GetInstance()->Malloc(n, alignment);
+//}
+//
+////delete[] using our allocator
+//void operator delete[](void * p)
+//{
+//	bool result = MemoryManager::GetInstance()->Free(p);
+//	assert(result);
+//}
+//
+////delete using debug log, won't get called.
+//void operator delete[](void * p, const char * msg) {
+//	DEBUGLOG(msg);
+//	bool result = MemoryManager::GetInstance()->Free(p);
+//	assert(result);
+//	
+//}
 
--- MonsterGame_WithEngine/MonsterEngine/MonsterEngine.h
+++ MonsterGame_WithEngine/MonsterEngine/MonsterEngine.h
@@ -12,16 +12,20 @@
 namespace MMEngine {
 	int RandomTrueOrFalse();
 };
-void * operator new(size_t n);
-
-void * operator new(size_t n, const char * msg);
-
-void operator delete(void * p);
-
-void operator delete(void * p, const char * msg);
-
-void * operator new[](size_t n);
-
-void operator delete[](void * p);
-
-void operator delete[](void * p, const char * msg);
+//void * operator new(size_t n);
+//
+//void * operator new(size_t n, const char * msg);
+//
+//void * operator new(size_t n, const uint8_t alignment);
+//
+//void operator delete(void * p);
+//
+//void operator delete(void * p, const char * msg);
+//
+//void * operator new[](size_t n);
+//
+//void * operator new[](size_t n, const uint8_t alignment);
+//
+//void operator delete[](void * p);
+//
+//void operator delete[](void * p, const char * msg);
